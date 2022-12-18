1251c0b1a4fac16398da8cf967cc4cae48c0d57e
Hollis <hollis.lawrence.moore@gmail.com>
2022-12-18 16:44:10 -0600

    Finished studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,42 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    if (!this.memberAlreadyAdded(memberName)) {
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+  save(name: string, member: object) {
+    if (!this.memberAlreadyAdded(name)) {
+      member['name'] = name;
+    }
+    this.memberBeingEdited = null;
+  }
+  
+  memberAlreadyAdded(name: string): boolean {
+    for (let i=0; i < this.crew.length; i++) {
+      if (this.crew[i].name = name) {
+         return true;
+      }
+    }
+    return false;
   }
 
 }
--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,3 +1,4 @@
+import { DomElementSchemaRegistry } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equiptment: string[] = [
+    "Habitat dome", "drones", "food containers", "oxygen tanks"
+  ]
+
+  itemBeingEdited: string = null;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+
+  save(newItem: string, originalItem: string) {
+    if (!this.equiptment.includes(newItem)) {
+      let index = this.equiptment.indexOf(originalItem);
+      this.equiptment.splice(index, 1, newItem);
+    }
+    
+    this.itemBeingEdited = null;
+  }
+
+  remove(item: string) {
+    let index = this.equiptment.indexOf(item);
+    this.equiptment.splice(index, 1);
+  }
+
+  add(item: string) {
+    if (!this.equiptment.includes(item)) {
+      this.equiptment.push(item);
+    }
   }
 
 }
